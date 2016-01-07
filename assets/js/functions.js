01b972105ad03cdda5d49e487d6bf198707ebf7b
ninapetrop <nina_petropoulos@student.uml.edu>
2016-01-06 19:37:49 -0500

    initial commit
    basic styles, layout, fixed position sidebar and post layouts

---
+++ assets/js/functions.js
@@ -0,0 +1,8 @@
+function mobileNav() {
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+}
+// a thing
